37bb8e25f36aea420ff85a6a5c87cf0fe0a5389a
so123789 <souparnikac09@gmail.com>
2026-08-24 00:19:53 +0530

    push axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+// });
+
+// // Attach JWT token to every request automatically
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://aicodereviewtool-fnls.onrender.com/api",
 });
 
-// Attach JWT token to every request automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
